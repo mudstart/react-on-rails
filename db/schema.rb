a7c33c97a6647b5376e3fcb335013f6e743f7405
Jose Miguel Maduro <maduro.josem@gmail.com>
2017-06-01 11:36:59 -0400

    add resource lift

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170601152152) do
+
+  create_table "lifts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.date     "date"
+    t.string   "liftname"
+    t.boolean  "ismetric"
+    t.integer  "weigthlifted"
+    t.integer  "repsperformed"
+    t.integer  "onerm"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
 end
